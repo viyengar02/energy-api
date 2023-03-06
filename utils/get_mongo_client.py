fd9e350e1e20901bb01ce64d18f2ce01701e314c
Aleksandar Aleksandrov <aleks@beyondix.net>
2023-03-05 22:45:58 -0500

    Implemented MongoDB config

--- utils/get_mongo_client.py
+++ utils/get_mongo_client.py
@@ -1,12 +1,12 @@
 from pymongo import MongoClient
+from utils.tools import get_config_file
 
 #Establish DB Connection
 def get_database(database_name: str):
-
-    # Provide the mongodb atlas url to connect python to mongodb using pymongo
-    CONNECTION_STRING =  "mongodb+srv://ava54:jvLsXWTf70tIkSij@cluster0.lsback5.mongodb.net/?retryWrites=true&w=majority" #"mongodb+srv://ava54:2Qxaj3GoMmUSENR7@cluster0.lsback5.mongodb.net/test"
     
     try:
+        mongo_config = get_config_file("mongodb.config.json")
+        CONNECTION_STRING = f"mongodb+srv://{mongo_config['username']}:{mongo_config['password']}@{mongo_config['url']}"
         # Create a connection using MongoClient. You can import MongoClient or use pymongo.MongoClient
         client = MongoClient(CONNECTION_STRING)
         # Create the database for our example (we will use the same database throughout the tutorial
---
+++ utils/tools.py
@@ -0,0 +1,19 @@
+import os
+import json 
+
+#Returns a specified config file as a dictionary
+def get_config_file(file_name: str):
+
+    try:
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
+        config_dir = os.path.join(parent_dir, 'config')
+
+        with open(f"{config_dir}/{file_name}", 'r') as f:
+            # Read the contents of the file into a string
+            config_dict = json.load(f)
+            return config_dict   
+    except Exception as e:
+        print(f"Exception occured in get_config_file: {e}")
+        return 1
+    
