b98e5147e0ae7b52f723eeeee432cd4b3424e88e
Cassius Ali Garcia <82051071+cassiusgarcia1@users.noreply.github.com>
2025-05-01 14:48:52 -0400

    Added ML + Twilio functions

--- services/ml_services.py
+++ services/ml_services.py
@@ -1,5 +1,9 @@
 from models.predictions import run_predictions, run_predictions_v2
 from fastapi import HTTPException
+import requests
+from requests.auth import HTTPBasicAuth
+from urllib.parse import urlencode
+import os
 
 def run_xgboost(days: int):
     try:
@@ -34,3 +38,50 @@ def run_compund_model(days: int):
     except Exception as error:
         print(f'Error occurred at load_xgboost: {error}')
         raise HTTPException(status_code=500, detail="Internal Server Error")
+
+
+    
+def send_pred_twilio(phonenum, predictions):
+    filtered_response = {}
+
+    for key, value in predictions.items():
+        if value.get("flagged_times"):
+            filtered_response[key] = {
+                "flagged_times": value["flagged_times"]
+            }
+
+    return send_sms(phonenum, filtered_response)
+
+def send_sms(phone_number, times):
+    url = f"https://api.twilio.com/2010-04-01/Accounts/{os.getenv('TWILIO_ACCOUNT_SID'}/Messages.json"
+    
+    auth = HTTPBasicAuth(os.getenv('TWILIO_ACCOUNT_SID'), os.getenv('TWILIO_AUTH_TOKEN')
+    
+    body = "🚨 Power Consumption Alerts 🚨\n\n"
+
+    for key, value in times.items():
+        body += f"* {key.replace('-', ' ').title()}:\n"
+
+        for i, time in enumerate(value['flagged_times'], 1):
+            body += f"   {i}. {time}\n"
+
+        body+="\n"
+    
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded'
+    }
+    
+    data = {
+        'To': phone_number,
+        'From': TWILIO_PHONE_NUMBER,
+        'Body': body
+    }
+    
+    response = requests.post(
+        url,
+        auth=auth,
+        headers=headers,
+        data=data
+    )
+    
+    return response.json()
