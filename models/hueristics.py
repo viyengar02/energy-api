9feae8949ebcc65066ac40470851acb646171ba2
Cassius Ali Garcia <82051071+cassiusgarcia1@users.noreply.github.com>
2025-05-13 19:27:08 -0400

    updated flagged times to use modeled tou hourly peco pricing

--- models/hueristics.py
+++ models/hueristics.py
@@ -29,11 +29,27 @@ def percent_err(df, target):
     """
     Returns hours to turn off system for given appliance based on threshold.
     """
+    peco_data = pd.DataFrame({
+    'price': [0.06122,0.04311,0.04311,0.04311,0.04311,0.04311,0.04311,0.06122,
+              0.06122,0.06122,0.06122,0.06122,0.06122,0.06122,0.06122,0.27978,
+              0.27978,0.27978,0.27978,0.06122,0.06122,0.06122,0.06122,0.06122],
+    'hour': pd.date_range(start='00:00:00', periods=24, freq='h').time.astype(str)
+    })
+
+    df_copy = df.copy()  # Avoid modifying original
+    df_copy['datetime'] = df_copy.index  # Preserve original datetime
+    df_copy["hour"] = df_copy.index.strftime('%H:%M:%S')  # Extract just the time
+
+    df_copy = df_copy.merge(peco_data, on='hour', how='left')
+    df_copy["pred_price"] = df_copy["prediction"] * df_copy["price"]
+    df_copy["avgs_price"] = df_copy["avgs"] * df_copy["price"]
     outlier_threshold = 20 # anything higer than this is probably just a bad prediction
     flag_threshold = 15 # anything higher than this should be flagged
-    pe = abs((df["prediction"] - df["avgs"]) / df["prediction"] * 100)
-    ind = pe[(pe < outlier_threshold) & (pe > flag_threshold)].index
-    return ind
+    # pe = abs((df["prediction"] - df["avgs"]) / df["prediction"] * 100)
+    epsilon = 1e-10
+    pe = abs((df_copy["pred_price"] - df_copy["avgs_price"]) / (df_copy["pred_price"] + epsilon) * 100)
+    flagged_mask = pe[(pe < outlier_threshold) & (pe > flag_threshold)].index
+    return df_copy.loc[flagged_mask, "datetime"]
 
 def get_flagged_times(model_results, target, days):
     # Load in our testing data (train only)
